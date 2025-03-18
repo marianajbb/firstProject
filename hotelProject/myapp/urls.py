369bc0088df6c8e3691f333fbcc94f13c6309c82
marianajbb <157728173+marianajbb@users.noreply.github.com>
2025-03-18 16:42:56 -0400

    Delete hotelProject directory

--- hotelProject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import employee_list, employee_create, employee_update, employee_delete
-
-urlpatterns = [
-    path('', employee_list, name='employee_list'),
-    path('create/', employee_create, name='employee_create'),
-    path('update/<str:employee_id>/', employee_update, name='employee_update'),
-    path('delete/<str:employee_id>/', employee_delete, name='employee_delete'),
-]
